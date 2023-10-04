56a73ba77ef9a6381ae47286d9ab4aac5e12a63e
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-04 17:12:35 -0500

    Remove TODO comments Add comments describing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,33 +21,24 @@ $(function () {
   const saveToast = new bootstrap.Toast(document.getElementById("save-toast"), {
     delay: 3000,
   });
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
   scheduleEl.on("click", (e) => {
     const target = $(e.target);
     const parent = target.parent();
+    // Ensure the target is a saveBtn inside a time-block
     if (!target.is(".saveBtn") || !parent.is(".time-block")) return;
     const description = parent.children(".description").val();
     const parentId = parseInt(parent.attr("id").split("hour-")[1]);
+    // Save schedule
     schedule[parentId - baseHour] = description;
-    console.log(parentId - baseHour);
     localStorage.setItem("schedule", JSON.stringify(schedule));
     saveToast.show();
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   const currentHour = now.hour();
   for (let i = baseHour; i < maxHour; i++) {
     let status = "present";
+    // If the hour is less than the current, it is in the past
+    // If the hour is greater than the current, it is in the future
+    // Otherwise, that hour is the present
     if (i < currentHour) status = "past";
     else if (i > currentHour) status = "future";
     let time = (i % 12) + (i > 11 ? "PM" : "AM");
@@ -62,14 +53,11 @@ $(function () {
       </div>`);
     scheduleEl.append(hourEl);
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // Update the schedule elements with the events from localStorage
   for (let i = 0; i < schedule.length; i++) {
     $(`#hour-${baseHour + i} .description`).val(schedule[i]);
   }
-  // TODO: Add code to display the current date in the header of the page.
+  // Display the current date in the header of the page.
   const currentDayEl = $("#currentDay");
   currentDayEl.text(now.format("dddd, MMMM D, YYYY"));
 });
