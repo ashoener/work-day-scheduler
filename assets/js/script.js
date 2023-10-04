8004ea4ef61d446de30e98ba9c0554dd90db05df
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-04 16:17:38 -0500

    Add base code for saving Load schedule on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef {import("dayjs").Dayjs} DayJS
+ */
+
+const baseHour = 9;
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -9,6 +15,12 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  $(".time-block .saveBtn").on("click", (e) => {
+    const parent = $(e.target).parent();
+    const description = parent.children(".description").val();
+    const parentId = parent.attr("id");
+    alert(description);
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -19,7 +31,17 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+  /**
+   * @type {Object.<string,string>[]}
+   */
+  const schedule = JSON.parse(localStorage.getItem("schedule")) || [];
+  for (let i = 0; i < schedule.length; i++) {
+    $(`#hour-${baseHour + i} .description`).val(schedule[i]);
+  }
   // TODO: Add code to display the current date in the header of the page.
+  /**
+   * @type {DayJS}
+   */
   const now = dayjs();
   const currentDayEl = $("#currentDay");
   currentDayEl.text(now.format("dddd, MMMM D, YYYY"));
